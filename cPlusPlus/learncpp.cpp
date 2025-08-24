224e9cab7be98cfb265ac96059cb012157f2f902
Saroj Paudel <sarozpaudel6@gmail.com>
2025-08-24 12:03:27 +0545

    c++ basics

---
+++ cPlusPlus/learncpp.cpp
@@ -0,0 +1,238 @@
+
+/*
+Strivers C++ Basics
+Video : https://youtu.be/EAR7De6Goz4?si=90UDysHidBqPl1fs
+
+*/
+
+
+
+
+
+// #include<iostream>
+// #include<math.h>
+// #include<string>
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// void printName(string);
+
+
+//pass by value
+// void doSomething (int n){
+//     cout << n << endl;
+//     n+=5;
+//     cout << n << endl;
+//     n+=5;
+//     cout << n << endl;
+
+// }
+
+
+// //pass by refernce
+// void doSomething(string &s){
+//     s[0]='Z';
+//     cout << s;
+// }
+
+int main(){
+    
+    // cout << "Hey Saroj !" << endl << "Hey nabin" << endl;
+    // cout<< "Hi kritagya!";
+    
+    // int x,y;
+    // cin >> x >> y;
+    // cout<< "Value of x: "<< x << " and y: "<<y;
+
+
+    // int x = 15;
+    // long x = 15;
+    // long long x =150000000;
+
+    //float , double
+    // float x = 5.6;
+    // float y = 6;
+    // cout<<"Value of y: "<< y;
+
+
+    // string s1,s2;
+    // cin >> s1>>s2;
+    // cout<< s1 << " " << s2;
+
+    // string str;
+    // getline(cin,str);
+    // cout << str;
+
+    //characters
+
+    // char ch;
+    // cin >> ch;
+    // cout<< ch;
+
+
+    // input age and check adult
+
+    // int age;
+    // cin>>age;
+    // if (age >= 18)
+    // cout << "you are adult";
+    // else
+    // cout << "you are not adult";
+
+
+    /*
+    
+    below 25 - f
+    25 to 44 - e
+    45 to 49 - d
+    50 to 59 - c
+    60 to 79 - b
+    80 to 100 - a
+    
+    */
+
+    // int marks;
+    // cin >>  marks;
+    
+    // if(marks < 25)
+    // cout << "F";
+    // else if (marks >= 25 && marks <= 44)
+    // cout<< "E";
+    // else if(marks >= 45 && marks <= 49)
+    // cout<< "D";
+    // else if (marks >= 50 && marks <=59)
+    // cout<< "C";
+    // else if (marks >= 60 && marks <= 79)
+    // cout << "B";
+    // else if (marks <= 100)
+    // cout<< "A";
+
+
+    // int age;
+    // cin >> age;
+    // if (age < 18)
+    // {
+    //     cout << "not eligible for the job";
+
+    // }else if(age <=57){
+    //     cout << "eligible for job";
+    //     if(age >= 55)
+    //     cout << "but retirement soon !";
+
+    // }else{
+    //     cout<< "retirement time !";
+    // }
+    
+
+    // int day;
+    // cin >> day;
+    //  switch (day)
+    //  {
+    //  case 1:
+    //     cout<< "Sunday";
+    //     break;
+     
+    //  case 2:
+    //     cout<< "Monday";
+    //     break;
+    //  case 3:
+    //     cout<< "Tuesday";
+    //     break;
+    //  case 4:
+    //     cout<< "Wednesday";
+    //     break;
+    //  case 5:
+    //     cout<< "Thursday";
+    //     break;
+    //  case 6:
+    //     cout<< "Friday";
+    //     break;
+    //  case 7:
+    //     cout<< "Saturday";
+    //     break;
+    
+    //  default:
+    //     break;
+    //  }
+
+
+
+    // int arr[5];
+    // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+    // arr[3] = 16;
+    // cout<< arr[3];
+
+    // int arr[3][5];
+    // arr[1][3] = 78;
+    // cout<< arr[1][2];
+
+
+    // string s = "Saroj";
+    // int len = s.size();
+    // s[len-1] = 'z';
+    // cout << s[len-1];
+
+
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout << i << endl;
+    // }
+    
+    // int i=1;
+    // while (i<=5)    
+    // {
+    //     cout << i <<endl;
+    //     i = i+1;
+    // }
+    
+
+    // int i =2;
+    // do {
+    //     cout << i <<endl;
+    //     i++;
+    // } while (i<=1);
+    
+
+    // void - no return
+    // return
+    // parameterized
+    // non parameterized
+    
+    // string name;
+    // cin >> name;
+    // printName(name);
+
+    // int num = 10;
+    // // pass by value
+    // doSomething(num);
+    // cout << num;
+
+// string str = "saroz";
+// doSomething(str);
+// cout<<str;
+
+ 
+// int arr[5];
+
+// for(int i=0;i<=4;i++){
+//     cin>>arr[i];
+// }
+
+// for(int i=0;i<=4;i++){
+//     cout<<arr[i]<<endl;
+// }
+
+
+
+cout << "Cooked by c++ !";
+
+
+return 0;
+}
+
+// void printName(string name){
+//     cout<<"hello "<< name<<" !";
+// }
